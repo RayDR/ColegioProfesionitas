4f434cced8bb2acf8b0ede0ab052a63faa001740
Ricardo Adalid <ricardo.benitez@correo.setab.gob.mx>
2021-03-08 14:01:52 -0600

    Validaciones de formulario registrar asociación

--- sources/js/administracion/registro.js
+++ sources/js/administracion/registro.js
@@ -59,67 +59,67 @@ function fn_guardar_registro() {
 	datos_asociacion = (datos_asociacion_aux) ? datos_asociacion_aux : datos_asociacion;
 	datos_colegio = (datos_colegio_aux) ? datos_colegio_aux : datos_colegio;
 
-	// datos_asociacion.forEach(function (dato, indice) {
-	// 	nombre = $(`#modal-form-registro-asc #${dato.name}`).data('nombre') ?
-	// 		$(`#modal-form-registro-asc #${dato.name}`).data('nombre') :
-	// 		dato.name;
-	// 	if (dato.value == '') {
-	// 		errores += `El campo <b><a href="#modal-form-registro-asc #${dato.name}">${nombre}</a></b> es requerido.<br>`;
-	// 		futil_validacion_input($(`#modal-form-registro-asc #${dato.name}`), false);
-	// 	} else if (dato.name == "rfc") {
-	// 		if (!futil_valida_rfc(dato.value)) {
-	// 			cole_errores += `El <b><a href="#modal-form-registro-asc #${dato.name}">${nombre}</a></b> no es válido.<br>`;
-	// 			futil_validacion_input($(`#modal-form-registro-asc #${dato.name}`), false);
-	// 		} else
-	// 			futil_validacion_input($(`#modal-form-registro-asc #${dato.name}`), true);
-	// 	} else
-	// 		futil_validacion_input($(`#modal-form-registro-asc #${dato.name}`), true);
-	// });
+	datos_asociacion.forEach(function (dato, indice) {
+		nombre = $(`#modal-form-registro-asc #${dato.name}`).data('nombre') ?
+			$(`#modal-form-registro-asc #${dato.name}`).data('nombre') :
+			dato.name;
+		if (dato.value == '') {
+			errores += `El campo <b><a href="#modal-form-registro-asc #${dato.name}">${nombre}</a></b> es requerido.<br>`;
+			futil_validacion_input($(`#modal-form-registro-asc #${dato.name}`), false);
+		} else if (dato.name == "rfc") {
+			if (!futil_valida_rfc(dato.value)) {
+				cole_errores += `El <b><a href="#modal-form-registro-asc #${dato.name}">${nombre}</a></b> no es válido.<br>`;
+				futil_validacion_input($(`#modal-form-registro-asc #${dato.name}`), false);
+			} else
+				futil_validacion_input($(`#modal-form-registro-asc #${dato.name}`), true);
+		} else
+			futil_validacion_input($(`#modal-form-registro-asc #${dato.name}`), true);
+	});
 
-	// select_asociacion.forEach(function (select, indice) {
-	// 	if ($(`#${select}`).val() == null) {
-	// 		nombre= $(`#modal-form-registro-asc #${select}`).data('nombre')?
-	// 				$(`#modal-form-registro-asc #${select}`).data('nombre'):
-	// 				select;
-	// 		errores += `Por favor, seleccione un <b><a href="#modal-form-registro-asc #${select}">${nombre}</a></b> válido.<br>`;
-	// 		futil_validacion_input($(`#modal-form-registro-asc #${select}`), false);
-	// 	} 
-	// });
+	select_asociacion.forEach(function (select, indice) {
+		if ($(`#${select}`).val() == null) {
+			nombre= $(`#modal-form-registro-asc #${select}`).data('nombre')?
+					$(`#modal-form-registro-asc #${select}`).data('nombre'):
+					select;
+			errores += `Por favor, seleccione un <b><a href="#modal-form-registro-asc #${select}">${nombre}</a></b> válido.<br>`;
+			futil_validacion_input($(`#modal-form-registro-asc #${select}`), false);
+		} 
+	});
 
-	// select_colegio.forEach(function (select, indice) {
-	// 	if ($(`#${select}`).val() == null) {
-	// 		nombre= $(`#modal-form-registro-cole #${select}`).data('nombre')?
-	// 				$(`#modal-form-registro-cole #${select}`).data('nombre'):
-	// 				select;
-	// 		cole_errores += `Por favor, seleccione un <b><a href="#modal-form-registro-cole #${select}">${nombre}</a></b> válido.<br>`;
-	// 		futil_validacion_input($(`#modal-form-registro-cole #${select}`), false);
-	// 	}
-	// });
+	select_colegio.forEach(function (select, indice) {
+		if ($(`#${select}`).val() == null) {
+			nombre= $(`#modal-form-registro-cole #${select}`).data('nombre')?
+					$(`#modal-form-registro-cole #${select}`).data('nombre'):
+					select;
+			cole_errores += `Por favor, seleccione un <b><a href="#modal-form-registro-cole #${select}">${nombre}</a></b> válido.<br>`;
+			futil_validacion_input($(`#modal-form-registro-cole #${select}`), false);
+		}
+	});
 
-	// datos_colegio.forEach(function (dato, indice) {
-	// 	nombre = $(`#modal-form-registro-cole #${dato.name}`).data('nombre') ?
-	// 	$(`#modal-form-registro-cole #${dato.name}`).data('nombre') :
-	// 	dato.name;
-	// 	if (dato.value == '') {
-	// 		if (dato.name != "cuenta") {
-	// 			cole_errores += `El campo <b><a href="#${dato.name}">${nombre}</a></b> es requerido.<br>`;
-	// 			futil_validacion_input($(`#modal-form-registro-cole #${dato.name}`), false);
-	// 		}
-	// 	} else if (dato.name == "rfc") {
-	// 		if (!futil_valida_rfc(dato.value)) {
-	// 			cole_errores += `El  <b><a href="#${dato.name}">${nombre}</a></b> no es valido.<br>`;
-	// 			futil_validacion_input($(`#modal-form-registro-cole #${dato.name}`), false);
-	// 		} else
-	// 			futil_validacion_input($(`#modal-form-registro-cole #${dato.name}`), true);
-	// 	} else if (dato.name == "curp") {
-	// 		if (!futil_valida_curp(dato.value)) {
-	// 			cole_errores += `La  <b><a href="#${dato.name}">${nombre}</a></b> no es valida.<br>`;
-	// 			futil_validacion_input($(`#modal-form-registro-cole #${dato.name}`), false);
-	// 		} else
-	// 			futil_validacion_input($(`#modal-form-registro-cole #${dato.name}`), true);
-	// 	} else
-	// 		futil_validacion_input($(`#modal-form-registro-cole #${dato.name}`), true);
-	// });
+	datos_colegio.forEach(function (dato, indice) {
+		nombre = $(`#modal-form-registro-cole #${dato.name}`).data('nombre') ?
+		$(`#modal-form-registro-cole #${dato.name}`).data('nombre') :
+		dato.name;
+		if (dato.value == '') {
+			if (dato.name != "cuenta") {
+				cole_errores += `El campo <b><a href="#${dato.name}">${nombre}</a></b> es requerido.<br>`;
+				futil_validacion_input($(`#modal-form-registro-cole #${dato.name}`), false);
+			}
+		} else if (dato.name == "rfc") {
+			if (!futil_valida_rfc(dato.value)) {
+				cole_errores += `El  <b><a href="#${dato.name}">${nombre}</a></b> no es valido.<br>`;
+				futil_validacion_input($(`#modal-form-registro-cole #${dato.name}`), false);
+			} else
+				futil_validacion_input($(`#modal-form-registro-cole #${dato.name}`), true);
+		} else if (dato.name == "curp") {
+			if (!futil_valida_curp(dato.value)) {
+				cole_errores += `La  <b><a href="#${dato.name}">${nombre}</a></b> no es valida.<br>`;
+				futil_validacion_input($(`#modal-form-registro-cole #${dato.name}`), false);
+			} else
+				futil_validacion_input($(`#modal-form-registro-cole #${dato.name}`), true);
+		} else
+			futil_validacion_input($(`#modal-form-registro-cole #${dato.name}`), true);
+	});
 
 	if (!errores && !cole_errores) {
 		futil_alerta('', '', "#ascociacion-errores"); 
